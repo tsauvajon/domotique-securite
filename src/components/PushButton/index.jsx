2031b0d8a970f4c77ef2f949673e767aa8a8cd5d
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-04-04 17:16:07 +0200

    Liste de bouton => liste produits

--- src/components/PushButton/index.jsx
+++ src/components/PushButton/index.jsx
@@ -1,16 +1,20 @@
 import React, { PropTypes } from 'react';
 import { Card, CardHeader, CardText } from 'material-ui/Card';
 // import FontIcon from 'material-ui/FontIcon';
+import Avatar from 'material-ui/Avatar';
+import { List, ListItem } from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
 import ProductSearch from '../ProductSearch';
 import './style.css';
 
 // const addIcon = <FontIcon className="material-icons md-48">settings_remote</FontIcon>;
 // const editIcon = <FontIcon className="material-icons md-48">create</FontIcon>;
 
-  // <CardActions expandable>
-  //   {addIcon}
-  //   {editIcon}
-  // </CardActions>
+// <CardActions expandable>
+//   {addIcon}
+//   {editIcon}
+// </CardActions>
 
 
 const PushButton = props => (
@@ -22,6 +26,19 @@ const PushButton = props => (
         showExpandableButton
       />
       <CardText expandable>
+        <List>
+          <Subheader>Produits</Subheader>
+          {
+            props.products.map(product => (
+              <ListItem
+                key={product.url}
+                primaryText={product.name}
+                leftAvatar={<Avatar src={product.img} />}
+              />
+            ))
+          }
+        </List>
+        <Divider />
         <ProductSearch />
       </CardText>
     </Card>
@@ -30,6 +47,11 @@ const PushButton = props => (
 
 PushButton.propTypes = {
   name: PropTypes.string.isRequired,
+  products: PropTypes.arrayOf(PropTypes.object),
+};
+
+PushButton.defaultProps = {
+  products: [],
 };
 
 export default PushButton;
--- src/components/PushButtons/index.jsx
+++ src/components/PushButtons/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
+import uuid from 'uuid/v4';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import PushButton from '../PushButton';
 import Header from '../Header';
@@ -9,9 +10,48 @@ class PushButtons extends Component {
   constructor(props) {
     super(props);
     const pushButtons = [
-      'Lessive',
-      'PQ',
-      'Produit vaisselle',
+      {
+        id: uuid(),
+        name: 'Lessive',
+        products: [
+          {
+            name: 'X Tra lessive liquide 1.89L',
+            price: 10.44,
+            url: 'https://www.amazon.fr/Tra-Lessive-Liquide-Marseille-Lavages/dp/B00WQRRRUG',
+            img: 'https://images-na.ssl-images-amazon.com/images/I/71P2R5xiiUL._SL1500_.jpg',
+          },
+          {
+            name: 'Décolor Stop Etui de 35',
+            price: 5.71,
+            url: 'https://www.amazon.fr/Eau-Ecarlate-Décolor-Stop-Lingettes/dp/B0091OVISY',
+            img: 'https://images-na.ssl-images-amazon.com/images/I/81SnnG2DT4L._SL1500_.jpg',
+          },
+        ],
+      },
+      {
+        id: uuid(),
+        name: 'Cuisine',
+        products: [
+          {
+            name: 'Set de 3 éponges Spontex',
+            price: 1.60,
+            url: 'https://www.amazon.fr/Spontex-Set-Eponges-Combinés-Grattantes/dp/B01KVK58SW',
+            img: 'https://images-na.ssl-images-amazon.com/images/I/71RO704nPiL._SL1500_.jpg',
+          },
+        ],
+      },
+      {
+        id: uuid(),
+        name: 'Toilettes',
+        products: [
+          {
+            name: 'Scottex papier toilette 16 rouleaux',
+            price: 4.49,
+            url: 'https://www.amazon.fr/Scottex-Papier-Toilette-16-Rouleaux/dp/B01MY82RQF',
+            img: 'https://images-na.ssl-images-amazon.com/images/I/81hBOGrCsGL._SL1500_.jpg',
+          },
+        ],
+      },
     ];
     this.state = {
       pushButtons,
@@ -22,15 +62,12 @@ class PushButtons extends Component {
     return { muiTheme: getMuiTheme() };
   }
 
-  // getButtons() {
-  //   const buttons = this.state.pushButtons.reduce((accumulateur, valeur) => (
-  //     accumulateur + <PushButton name={valeur} />
-  //   ));
-  //   return buttons;
-  // }
-
+  // Gère le clic sur le Floating Action Button
   handleTouchTap() {
-    const pushButtons = this.state.pushButtons.concat('Nouveau bouton');
+    const pushButtons = this.state.pushButtons.concat({
+      id: uuid(),
+      name: 'Bouton Push',
+    });
     this.setState({
       pushButtons,
     });
@@ -40,13 +77,23 @@ class PushButtons extends Component {
     return (
       <div>
         <Header title={'Boutons Push'} />
-        {this.state.pushButtons.map(
-          btn => (<PushButton name={btn} />),
-        )}
+        <center>
+          {
+            this.state.pushButtons.map(
+            btn => (
+              <PushButton
+                key={`push-btn-${btn.name}-${btn.id}`}
+                name={btn.name}
+                products={btn.products}
+                onTouchTap={() => {}}
+              />),
+            )
+          }
+        </center>
         <FloatingActionButton
           style={{
             marginRight: 20,
-            position: 'absolute',
+            position: 'fixed',
             bottom: '2%',
             right: '2%',
           }}
