7de94f769620c73ddd8e2c9f7d993e87add9c722
cesar <cesar.bosc@epsi.fr>
2017-04-05 14:15:35 +0200

    ajout connexion

--- src/components/Amazon/index.jsx
+++ src/components/Amazon/index.jsx
@@ -4,15 +4,17 @@ let searchProducts;
 let products;
 let productchoose;
 
-const client = amazon.createClient({
+const Client = amazon.createClient({
   awsId: 'AKIAI7LNXV6L7HWJB3XA',
   awsSecret: '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',
   awsTag: 'domopush',
 });
 
-products(function* generator() {
-  productchoose = client.itemSearch({ keywords: 'productSearchAutoComplete', searchIndex: 'All' });
+products(function* bar() {
+  productchoose = Client.itemSearch({ keywords: 'productSearchAutoComplete', searchIndex: 'All' });
 
   searchProducts = yield [productchoose];
   console.log(searchProducts);
 })();
+
+export default Client;
